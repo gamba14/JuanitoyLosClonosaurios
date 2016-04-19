c481110be1b4f3e046baad5ae767cb1c7994c0ff
gamba14 <gambirassi.a@gmail.com>
2016-04-19 12:18:18 -0300

    Update tp1_ssl.py
    Mejorando de a poco

--- tp1_ssl.py
+++ tp1_ssl.py
@@ -5,20 +5,42 @@
 # 0a1b2&0#
 # 1a1b0#
 # 2a2b2#
-# especificacion del automata
-# deberia poder parsear el texto que se da como entrada
 # de la linea 1 a 4 se dan los elementos fijos que son 
 # linea 1 : el conjunto de estado
 # linea 2: el conjunto de entradas
 # linea 3 : el estado inicial
 # linea 4 : los estados aceptados
-# a continuacion abro el archivo de texto y comiendo a parsear
-entrada = open("ej-especif-aut.txt", "r") 
-#sabiendo que de la linea 1 a 4 estan los datos que son fijos hacer
+###################################################################################################
+# a continuacion abro el archivo de texto 
+###################################################################################################
+entrada = open("ej-especif-aut.txt", "r")
+###################################################################################################
+
+
+
+#deberia definir como son las transiciones ya que las voy a necesitar cuando haga clausura lambda
+
+
+###################################################################################################
+#funcion clausura lambda
+###################################################################################################
+def cllambda (estados):
+    l=estados
+    while length(l) > 0: #aca quiero asumir que la lista l se ira consumiendo a medida que itere
+    	estados.pop([])
+
+
+
+###################################################################################################
+# funcion mover
+###################################################################################################
+def mover (estado,sigma): #sea sigma un elemento de sigma :P
+
+
+#sabiendo que de la linea 1 a 4 estan los datos que son fijos hacer (va mas arriba lo se)
 for i in range (4):
     content=entrada.readline()
     print content
 # parseo la funcion de transicion
 # se que el eol es un hash
-
     
