49341c540027990cc5afb51882abd77ebbc025d9
EmmanuelPL <emmanuel.posadasl@gmail.com>
2016-04-19 22:07:14 -0300

    - Agregado script para que ejecute el main si llama directamente (osea que no es importado) - El definidos los parametros para el script - Agregados metodos para el parseo del archivo (la mayoria sin implementar) - Agregado bang cosa "#!/usr/bin/python"

--- tp1_ssl.py
+++ tp1_ssl.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+
+import os.path
+import fileinput
+import sys, getopt
+
+
 # {0,1,2}
 # {a,b}
 # {0}
@@ -10,37 +17,124 @@
 # linea 2: el conjunto de entradas
 # linea 3 : el estado inicial
 # linea 4 : los estados aceptados
-###################################################################################################
-# a continuacion abro el archivo de texto 
-###################################################################################################
-entrada = open("ej-especif-aut.txt", "r")
-###################################################################################################
+
+def parseStates(asf, line):
+    """ Parse the line with the states"""
+    pass
 
 
+def parseInputs(asf, line):
+    """ Parse the line with the inputs"""
+    pass
 
-#deberia definir como son las transiciones ya que las voy a necesitar cuando haga clausura lambda
+
+def parseInitState(asf, line):
+    """ Parse the line with the Init state"""
+    pass
 
 
-###################################################################################################
-#funcion clausura lambda
-###################################################################################################
+def parseFinalStates(asf, line):
+    """ Parse the line with the final/accepted states"""
+    pass
+
+
+def parseTransition(asf, line):
+    """ Parse one line with a part of the transition function """
+    pass
+
+
+def parseFile(path):
+    """Parse the file, path must be the path to a existing text file"""
+
+    # automata
+    asf = { 
+        "states": set([]),
+        "inputs": set([]),
+        "init": 0,
+        "final": set([]),
+        "transtion": []
+    }
+
+    # define a dictionary becase are cool and don't have a swith case 
+    parsers = {
+        0: parseStates,
+        1: parseInputs,
+        2: parseInitState,
+        3: parseFinalStates
+    }
+
+    # read each line in the file and parse it
+    for lineNumber, line in enumerate(fileinput.input(path)):
+        parser = parsers.get(lineNumber, parseTransition)
+        parser(asf, line)
+
+    # return the automata
+    return asf
+
+
 def cllambda (estados):
+	""" Funcion clausura lambda """
     l=estados
     while length(l) > 0: #aca quiero asumir que la lista l se ira consumiendo a medida que itere
     	estados.pop([])
 
 
-
-###################################################################################################
-# funcion mover
-###################################################################################################
 def mover (estado,sigma): #sea sigma un elemento de sigma :P
+	""" funcion mover """
+	pass
+    
+
+def isValid(asd, input):
+    """Check if the input string is acepted by the Automata"""
+    #TODO: implementar, devolver True o False
+    pass
 
 
-#sabiendo que de la linea 1 a 4 estan los datos que son fijos hacer (va mas arriba lo se)
-for i in range (4):
-    content=entrada.readline()
-    print content
-# parseo la funcion de transicion
-# se que el eol es un hash
+def main(argv):
+    """Main process"""
+
+    path = ""
+    inputString = ""
+    withString  = False
+
+    # Parse arguments
+    try:
+        opts, args = getopt.getopt(argv,"ha:s:",["automata=","string="])
+
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>' #TODO: cambiar
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>' #TODO: cambiar
+            sys.exit()
+
+        elif opt in ("-a", "--automata"):
+            path = arg
+
+        elif opt in ("-s", "--string"):
+            inputString = arg
+            withString  = True
+
+    # Parse the file if exist
+    if (os.path.exists(path) && os.path.isfile(path))
+        asf = parseFile(path)
+    else
+        print "El archivo no es valido" # TODO: mejorar mensaje de error
+
+    if (withString)
+        if (isValid(asf, inputString))
+            print "La cadena es aceptada por el automata" #TODO: mejorar el mensaje
+        else
+            print "La cadena NO es aceptada por el automata" #TODO: mejorar el mensaje
+    else
+        # TODO: leer la cadena que ingresa el usuario y chekear si la acepta el automata
+        print "implementame (?)"
+
+
+if __name__ == "__main__":
+    # the code being run directly, other wise is imported
+    if __name__ == "__main__":
+        main(sys.argv[1:])
     
