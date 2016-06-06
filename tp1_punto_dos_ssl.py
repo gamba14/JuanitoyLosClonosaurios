936875a2e2a7a272cb6cc657a4879fb62d2e9a7d
EmmanuelPL <emmanuel.posadasl@gmail.com>
2016-06-06 18:42:15 -0300

    Agregado archivo para el punto dos con la estructura basica

---
+++ tp1_punto_dos_ssl.py
@@ -0,0 +1,100 @@
+﻿#!/usr/bin/python
+
+import os.path
+import fileinput
+import sys, getopt
+
+# TODO Definir funciones y escribir el comportamiento esperado
+# TODO cambiar los comentarios donde corresponda
+# TODO importar funciones del punto uno
+
+def main(argv):
+    """Proceso principal"""
+
+    # Variables que van a contener la informacion obtenida de los parametros
+    path = ""
+    inputString = ""
+    withString  = False
+
+    # Interpreto/Parseo/(?) los parametros
+    try:
+        # Obtiene las opciones (-h, -a, --automata, -s, --string)
+        # los argumentos correspondientes a cada uno de ellos
+        opts, args = getopt.getopt(argv,"ha:s:",["automata=","string="])
+
+    except getopt.GetoptError:
+        # Si hay alguna opcion invalida
+
+        # Se informa la forma correcta de uso
+        print ('tp1_punto_dos_ssl.py -i <inputfile> -o <outputfile>')
+        
+        # Y se finaliza el scritp informando un error 
+        # Cuando se finaliza con cualquier nuemero distinto de 0 se considerar
+        # que el programa finalizo con errores, en esta caso 2
+        sys.exit(2)
+
+    # Por cada par ordenado de opciones-argumentos obtenidos
+    for opt, arg in opts:
+
+        # Si la opcion es '-h' 
+        if opt == '-h':
+
+            # se informa la forma de uso correcto
+            print ('tp1_punto_dos_ssl.py -a <archivo_automata> -s <cadena>')
+
+            # Se finaliza el scritp sin error
+            sys.exit()
+
+        # Si la opcion es '-a' o '--automata'
+        elif opt in ("-a", "--automata"):
+            # el argumento asociado es la ruta al archivo txt con el automata
+            # a parsear/interpretar/(?)
+            path = arg
+
+        # Si la opcion es '-s' o '--string'
+        elif opt in ("-s", "--string"):
+            # el argumento asociado es la cadena que se debe verificar si es 
+            # aceptada por el automata
+            inputString = arg
+
+            # Uso una bandera para informar que la cadena ya fue proporcionada 
+            # via argumento
+            withString  = True
+
+    # Si la ruta es valida y corresponde a un archivo
+    if (os.path.exists(path) and os.path.isfile(path)):
+
+        # Parseo/Interpreto/(?) el archivo (como automata)
+        asf = parseFile(path)
+
+        # Imprimo el resultado del parseo (me canse)
+        print (asf) 
+
+        # Si se dispone a cadena a comprobar
+        if (withString):
+
+            # Verifico que la cadena sea aceptada
+            if (isValid(asf, inputString)):
+                # Informo que es aceptada
+                print ("La cadena es aceptada por el automata") #TODO: mejorar el mensaje
+
+            else:
+                # Informo que NO es aceptada
+                print ("La cadena NO es aceptada por el automata") #TODO: mejorar el mensajepyt
+        else:
+            # leer las cadena que ingresa el usuario y chekear si cada una es 
+            # aceptada por el automata
+            print ("implementame (?)") # TODO implementar
+
+    else:
+        # Si la ruta no es valida o no corresponde a un archivo
+        print ("El archivo no es valido") # TODO: mejorar mensaje de error
+
+
+# Si el scritp es llamado directamente (Osea NO es importado como libreria)
+if __name__ == "__main__":
+    # Ejecuto el proceso principal con los argumentos exepto el primero que es 
+    # el nombre del scritp
+    main(sys.argv[1:])
+    
+    
